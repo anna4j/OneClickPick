05cfaa963bab6b474cdc6d8c28ac2b24463b5518
parjanyaacoder <padityashukla@gmail.com>
2020-07-13 14:26:13 +0530

    Improved cart.js

--- Cart/cart.js
+++ Cart/cart.js
@@ -1,6 +1,11 @@
 // Coupon Code pop up message
 
 var couponCd = document.querySelector('#button-addon3');
+var quantityList = document.querySelectorAll('.quantity');
+for (var i = quantityList.length - 1; i >= 0; i--) {
+    console.log(quantityList[i].value);
+}
+
 
 couponCd.addEventListener('click' , function(){
     var copCdvalue = document.querySelector('.form-control').value;
@@ -12,62 +17,56 @@ couponCd.addEventListener('click' , function(){
 });
 
 
-function incrementValue()
-{
-    var value = parseInt(document.getElementById("item1").value, 10);
-    value = isNaN(value) ? 0 : value;
+function incrementQuantity(i)
+{  i = parseInt(i);
+  var value = quantityList[i-1].value;
+  
+ value = isNaN(value) ? 0 : value;
     if(value<10){
         value++;
-            document.getElementById("item1").value = value;     
-    }
+
+    quantityList[i-1].value = value;
+     
 }
-function decrementValue()
+
+}
+
+
+function decrementQuantity(i)
 {
-    var value = parseInt(document.getElementById("item1").value, 10);
+    i = parseInt(i);
+    var value = quantityList[i-1].value;
     value = isNaN(value) ? 0 : value;
     if(value>1){
         value--;
-            document.getElementById("item1").value = value;
+        quantityList[i-1].value = value;
     }
+    
+}
+
+
+
+var priceList = document.querySelectorAll('.price');
+for (var i = priceList.length - 1; i >= 0; i--) {
+    console.log(priceList[i].value);
+}
+
+
+
+var totalPrice = parseInt(document.getElementById('totalPrice').value);
+totalPrice = isNaN(totalPrice) ? 0 :totalPrice;
+
+
+function updatePrice()
+{   var sum=0;
+    
+    for (var i = 0 ; i <= quantityList.length - 1; i++) {
+        sum = sum + quantityList[i].value*priceList[i].value;
+    } 
+    
+    return sum;
 
 }
 
-function incrementValue2()
-{
-    var value = parseInt(document.getElementById("item2").value, 10);
-    value = isNaN(value) ? 0 : value;
-    if(value<10){
-        value++;
-            document.getElementById("item2").value = value;     
-    }
-}
-function decrementValue2()
-{
-    var value = parseInt(document.getElementById("item2").value, 10);
-    value = isNaN(value) ? 0 : value;
-    if(value>1){
-        value--;
-            document.getElementById("item2").value = value;
-    }
-
-}
-
-function incrementValue3()
-{
-    var value = parseInt(document.getElementById("item3").value, 10);
-    value = isNaN(value) ? 0 : value;
-    if(value<10){
-        value++;
-            document.getElementById("item3").value = value;     
-    }
-}
-function decrementValue3()
-{
-    var value = parseInt(document.getElementById("item3").value, 10);
-    value = isNaN(value) ? 0 : value;
-    if(value>1){
-        value--;
-            document.getElementById("item3").value = value;
-    }
-
-}
+totalPrice.value = updatePrice();
+console.log(updatePrice());
